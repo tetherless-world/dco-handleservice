6de391eb037516ad0cf5f526296c3d753b088ff2
mrpatrickwest <westp@rpi.edu>
2015-11-24 15:14:12 -0500

    Reading handles from file instead of using array of strings

--- scripts/removehandle.php
+++ scripts/removehandle.php
@@ -2,7 +2,7 @@
 
 include_once( "utils.php" ) ;
 
-$url = "http://localhost:8080/dcohandleservice/services/handles" ;
+$url = "http://deepcarbon.tw.rpi.edu:8080/dcohandleservice/services/handles" ;
 
 if( $argc > 1 )
 {
@@ -11,25 +11,35 @@ if( $argc > 1 )
 }
 else
 {
-    $ids = array(
-                  "x1",
-                  "x2"
-    ) ;
+    if( file_exists( "toremove" ) )
+    {
+        $ids = file( "toremove" ) ;
+    }
+    else
+    {
+        print( "no dco-id specified\n" ) ;
+        print( "Usage: $argv[0] [<handle1> [<handle2]]\n" ) ;
+        print( "    if no handles specified then loads file \"toremove\"\n" ) ;
+        exit( 1 ) ;
+    }
 }
 
 // remove the id
 foreach( $ids as $id )
 {
-    print( "REMOVING $id\n" ) ;
-    $cmd = "$url/remove" ;
-    $data = "<handle><id>$id</id><type>URL</type><value></value></handle>" ;
-    $result = getResult( $cmd, $data ) ;
-    if( $result === false )
+    if( strlen( $id ) > 5 && substr( $id, 0, 5 ) == "11121" )
     {
-        print( "Failed to remove the dcoid\n" ) ;
-        exit( 1 ) ;
+        print( "REMOVING $id\n" ) ;
+        $cmd = "$url/remove" ;
+        $data = "<handle><id>$id</id><type>URL</type><value></value></handle>" ;
+        $result = getResult( $cmd, $data ) ;
+        if( $result === false )
+        {
+            print( "Failed to remove the dcoid\n" ) ;
+            exit( 1 ) ;
+        }
+        print( "result = $result\n" ) ;
     }
-    print( "result = $result\n" ) ;
 }
 ?>
 
